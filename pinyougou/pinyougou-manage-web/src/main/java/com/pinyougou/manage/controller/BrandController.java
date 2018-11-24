f72fa1b0cc6da8ed47cb38992c2630527264ee04
JinXinDe <995847170@qq.com>
2018-11-24 21:11:12 +0800

    新增和修改操作

--- pinyougou/pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/BrandController.java
+++ pinyougou/pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/BrandController.java
@@ -3,6 +3,8 @@ package com.pinyougou.manage.controller;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.pinyougou.pojo.TbBrand;
 import com.pinyougou.sellergoods.service.BrandService;
+import com.pinyougou.vo.PageResult;
+import com.pinyougou.vo.Result;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -15,9 +17,73 @@ public class BrandController {
     @Reference
     private BrandService brandService;
 
+    @PostMapping("/update")
+    public Result update(@RequestBody TbBrand brand) {
+        try {
+            brandService.update(brand);
+            return Result.ok("更新品牌成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Result.fail("更新品牌失败");
+    }
+
+    /**
+     * 根据品牌主键查询品牌
+     * @param id 品牌主键
+     * @return 品牌
+     */
+    @GetMapping("/findOne")
+    public TbBrand findOne(Long id) {
+        return brandService.findOne(id);
+    }
+
+    /**
+     * 保存品牌数据到数据库中
+     * @param brand 品牌
+     * @return 操作结果
+     */
+    @PostMapping("/add")
+    public Result add(@RequestBody TbBrand brand) {
+        try {
+            brandService.add(brand);
+            return Result.ok("新增成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return  Result.fail("新增失败");
+    }
+
+    /**
+     * 根据页号和页大小查询品牌列表
+     * @param page 页号
+     * @param rows 页大小
+     * @return 分页结果
+     */
+    @GetMapping("/findPage")
+    public PageResult findPage(@RequestParam(defaultValue = "1") Integer page, @RequestParam(defaultValue = "10") Integer rows) {
+        return brandService.findPage(page, rows);
+    }
+
+    /**
+     * 根据页号和页大小查询品牌列表
+     * @param page 页号
+     * @param rows 页大小
+     * @return 品牌列表
+     */
+    @GetMapping("/testPage")
+    public List<TbBrand> testPage(@RequestParam(defaultValue = "1") Integer page, @RequestParam(defaultValue = "10") Integer rows) {
+        //return brandService.testPage(page,rows);
+        return (List<TbBrand>) brandService.findPage(page, rows);
+    }
+
+    /**
+     * 查询所有品牌数据
+     * @return 品牌列表json格式字符串
+     */
     @GetMapping("/findAll")
     public List<TbBrand> findAll() {
-        return brandService.queryAll();
-
+        //return brandService.queryAll();
+        return brandService.findAll();
     }
 }
